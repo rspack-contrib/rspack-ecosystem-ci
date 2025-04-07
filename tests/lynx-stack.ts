37417aedca89b2cd3bb091c9f6dcfe5602b86397
Qingyu Wang <40660121+colinaaa@users.noreply.github.com>
2025-04-07 20:55:03 +0800

    fix: clone lynx-stack to tmpdir

--- tests/lynx-stack.ts
+++ tests/lynx-stack.ts
@@ -1,9 +1,15 @@
+import { mkdtemp } from 'node:fs/promises'
+import { tmpdir } from 'node:os'
+
 import { runInRepo } from '../utils'
 import { RunOptions } from '../types'
 
 export async function test(options: RunOptions) {
+	const tmp = await mkdtemp(tmpdir())
+
 	await runInRepo({
 		...options,
+		workspace: tmp,
 		repo: 'lynx-family/lynx-stack',
 		branch: process.env.LYNX_STACK_REF ?? 'main',
 		beforeBuild: `rustup target add wasm32-unknown-unknown`,
