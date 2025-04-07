7083d117b935fcf44e5dacaf2d66f8c98395493b
Qingyu Wang <40660121+colinaaa@users.noreply.github.com>
2025-04-07 21:52:37 +0800

    ci: enable lynx-stack suite (#76)
    * ci: enable lynx-stack suite
    
    * fix: setup nodejs
    
    * fix: try use link protocol with pnpm@10
    
    * Revert "fix: try use link protocol with pnpm@10"
    
    This reverts commit a718166e23278e9a90e0fa55e95d71e33a285dd1.
    
    * WIP: try colinaaa:colin/0404/uqr
    
    * fix: path
    
    * fix: remove beforeInstall
    
    * fix: add `--silent`
    
    * fix: overriding rspack-test-tools
    
    * fix: typo
    
    * chore: retry
    
    * fix: use node22 in self test
    
    * fix: clone lynx-stack to tmpdir
    
    * fix: tmpdir

--- tests/lynx-stack.ts
+++ tests/lynx-stack.ts
@@ -1,13 +1,21 @@
+import { mkdtemp } from 'node:fs/promises'
+import { tmpdir } from 'node:os'
+import { join } from 'node:path'
+
 import { runInRepo } from '../utils'
 import { RunOptions } from '../types'
 
 export async function test(options: RunOptions) {
+	const tmp = await mkdtemp(join(tmpdir(), 'lynx-stack-'))
+
 	await runInRepo({
 		...options,
+		workspace: tmp,
 		repo: 'lynx-family/lynx-stack',
 		branch: process.env.LYNX_STACK_REF ?? 'main',
+		beforeBuild: `rustup target add wasm32-unknown-unknown`,
 		// TODO(colinaaa): enable Lynx for Web tests
 		build: 'pnpm turbo build',
-		test: 'test',
+		test: 'pnpm run test --silent',
 	})
 }
