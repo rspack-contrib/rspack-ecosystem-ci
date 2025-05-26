cd2ba7bcc5846e601f722d5b5d211b591a4f62ac
Cong-Cong <dacongsama@live.com>
2025-05-26 10:34:53 +0800

    fix

--- tests/nextjs.ts
+++ tests/nextjs.ts
@@ -5,7 +5,6 @@ import { RunOptions } from '../types'
 
 export async function test(options: RunOptions) {
 	const { workspace, shardPair, rspackPath } = options;
-	const rspackCorePath = path.join(rspackPath, 'packages/rspack');
 
 	await runInRepo({
 		...options,
@@ -13,11 +12,25 @@ export async function test(options: RunOptions) {
 		branch: 'canary',
 		build: ['build'],
 		beforeTest: async () => {
-			const nextRspackPath = path.join(workspace, 'next.js/packages/next-rspack');
-			const nextRspackPkgPath = path.join(nextRspackPath, 'package.json');
-			const pkg = JSON.parse(fs.readFileSync(nextRspackPkgPath, 'utf-8'));
-			pkg.dependencies['@rspack/core'] = `${rspackCorePath}`;
-			fs.writeFileSync(nextRspackPkgPath, JSON.stringify(pkg, null, 2));
+			const rspackCorePath = path.join(rspackPath, 'packages/rspack');
+
+			const nextWorkspaceDir = path.join(workspace, 'next.js');
+
+			const nextRspackDir = path.join(nextWorkspaceDir, 'next.js/packages/next-rspack');
+			const nextRspackPkgPath = path.join(nextRspackDir, 'package.json');
+			const nextRspackPkg = JSON.parse(fs.readFileSync(nextRspackPkgPath, 'utf-8'));
+			nextRspackPkg.dependencies['@rspack/core'] = `${rspackCorePath}`;
+			fs.writeFileSync(nextRspackPkgPath, JSON.stringify(nextRspackPkg, null, 2));
+
+			const getRspackPath = path.join(nextWorkspaceDir, 'packages/next/src/shared/lib/get-rspack.ts');
+			const getRspackContent = fs.readFileSync(getRspackPath, 'utf-8');
+			const replacedGetRspackContent = getRspackContent.replace("require.resolve('@rspack/core'", `require.resolve('${rspackCorePath}'`);
+			fs.writeFileSync(getRspackPath, replacedGetRspackContent);
+
+			const compiledWebpackPath = path.join(nextWorkspaceDir, 'packages/next/src/compiled/webpack/webpack.js');
+			const compiledWebpackContent = fs.readFileSync(compiledWebpackPath, 'utf-8');
+			const replacedCompiledWebpackContent = compiledWebpackContent.replace("require('@rspack/core')", `require('${rspackCorePath}')`);
+			fs.writeFileSync(compiledWebpackPath, replacedCompiledWebpackContent);
 		},
 		test: async () => {
 			const env = {
