c3645ee87603ccf1c5c1e3e915f8d5fa0405c6b5
neverland <chenjiahan.jait@bytedance.com>
2025-07-05 11:30:28 +0800

    fix: enable strict-peer-dependencies

--- utils.ts
+++ utils.ts
@@ -537,7 +537,7 @@ async function applyPackageOverrides(
 
 	// use of `ni` command here could cause lockfile violation errors so fall back to native commands that avoid these
 	if (pm === 'pnpm') {
-		await $`pnpm install --prefer-frozen-lockfile --prefer-offline --strict-peer-dependencies false`
+		await $`pnpm install --prefer-frozen-lockfile --prefer-offline`
 	} else if (pm === 'yarn') {
 		await $`yarn install`
 	} else if (pm === 'npm') {
